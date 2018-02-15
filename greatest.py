c9a23f6246ee935111a2c3586faa3b0f0f99bb6a
vigneshsadasivam <36299168+vigneshsadasivam@users.noreply.github.com>
2018-02-15 14:18:54 +0530

    Update greatest.py

--- greatest.py
+++ greatest.py
@@ -1,18 +1,10 @@
 a=int(input("enter the number a"))
-
 b=int(input("enter the number b"))
-
 c=int(input("enter the number c"))
-
 if(a>c and a>c):
-    
 	print("a is the greatest")
-
 elif(b>c and b>a):
-  
-	  print("b is the greatest")
-
+	print("b is the greatest")
 else:
- 
-	 print("c is the greatest")
+	print("c is the greatest")
     
