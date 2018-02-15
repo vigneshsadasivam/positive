5ea86153a458c1e5772062eff01131c23ce354d0
vigneshsadasivam <36299168+vigneshsadasivam@users.noreply.github.com>
2018-02-15 15:00:11 +0530

    Update prime.py

--- prime.py
+++ prime.py
@@ -1,25 +1,13 @@
 num=int(input("enter a number"))
-
 if(num==2):
-
   print("it is prime")
-
 elif num > 2:
-
-   for i in range(2,num):
-
-       if (num % i) == 0:
-
-           print("is not a prime number")
-
-           break
-
-       else:
-
-         print("it is prime")
-
-         break
-
+  for i in range(2,num):
+    if (num % i) == 0:
+      print("is not a prime number")
+      break
+    else:
+      print("it is prime")
+      break
 else:
-
   print("it is not a prime")
