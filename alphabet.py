9263c6608f8e78b8d4d5a87d541e1db37fd6434e
vigneshsadasivam <36299168+vigneshsadasivam@users.noreply.github.com>
2018-02-15 14:12:25 +0530

    Update alphabet.py

--- alphabet.py
+++ alphabet.py
@@ -1,9 +1,5 @@
 n=input("enter the character")
-
 if((n>='a' and n<='z') or (n>='A' and n<='Z')):
-
-			print("yes it is a alphabet")
-
+	print("alphabet")
 else:
-			
-	print( "no it is not an alphabet")
+	print( "no")
